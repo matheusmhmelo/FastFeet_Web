147440ca1158d6764b1b18612bc0c9b752109852
matheusmelo <ma_hrm@hotmail.com>
2020-03-17 21:03:42 -0300

    Initial commit and Login Page

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Avatar from 'react-avatar';
+// import { useSelector } from 'react-redux';
+// import { Link } from 'react-router-dom';
+
+// import Notifications from '~/components/Notifications';
+
+// import logo from '~/assets/logo-purple.svg';
+import { Container, Content } from './styles';
+
+export default function Header() {
+  // const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Content />
+    </Container>
+  );
+}
